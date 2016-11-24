11ceacae091321382c5e9fcfd8f625a25f93bad2
Jose Agustin Villalobos <joseavv4@gmail.com>
2016-11-24 01:19:45 -0400

    add semicolons

--- server/server.js
+++ server/server.js
@@ -1,43 +1,40 @@
-'use strict'
-// Core modules
-const path = require('path')
-const http = require('http')
-// third party modules
-const express = require('express')
-const logger = require('morgan')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const logger = require('morgan');
 
-// create instance of express
-const port = process.env.PORT || 8080
-const app = express()
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(logger('dev'))
-app.use('/libs', express.static(path.join(__dirname, '../node_modules')))
-app.use('/dist', express.static(path.join(__dirname, '../dist')))
-app.use(express.static(path.join(__dirname, '../game')))
+app.use(logger('dev'));
+app.use('/libs', express.static(path.join(__dirname, '../node_modules')));
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../game')));
 
-app.get('*', html5Mode)
-
-function html5Mode(req, res) {
-  res.sendFile(path.join(__dirname, '../game/index.html'))
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../game/index.html'));
+});
 
 // error handlers
-app.use(notFound)
-app.use(errorStatus)
+app.use(notFound);
+app.use(errorStatus);
 
 function notFound(req, res, next) {
-  let err = new Error('Not Found')
-  err.status = 404
-  next(err)
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 }
 
 function errorStatus(err, req, res) {
-  res.status(err.status || 500)
-  res.end(JSON.stringify({message: err.message, error: {}}))
+  res.status(err.status || 500);
+  res.end(JSON.stringify({
+    message: err.message,
+    error: {},
+  }));
 }
 
 const server = http
   .createServer(app)
-  .listen(port, function() {
-    console.log('Server running on port: ' + server.address().port)
-  })
+  .listen(port, () => {
+    console.log(`Server running on port: ${server.address().port}`);
+  });
